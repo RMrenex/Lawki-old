117b7c5af965826af84291fece38bab09c1c9f6e
BuildTools <unconfigured@null.spigotmc.org>
2021-03-30 23:02:57 +0200

    Rename AddCategorieForm to CategorieForm

---
+++ forms/CategorieForm.designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace LowaPass
+{
+    partial class CategorieForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.leftPanel = new System.Windows.Forms.Panel();
+            this.rightPanel = new System.Windows.Forms.Panel();
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.bottomPanel = new System.Windows.Forms.Panel();
+            this.centerPanel = new System.Windows.Forms.Panel();
+            this.btValideCategorie = new System.Windows.Forms.Button();
+            this.centerRightPanel = new System.Windows.Forms.Panel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.inputPassword = new System.Windows.Forms.TextBox();
+            this.subPanelWarning = new System.Windows.Forms.Panel();
+            this.labelWarning = new MetroFramework.Controls.MetroLabel();
+            this.centerLeftPanel = new System.Windows.Forms.Panel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.inputLogin = new System.Windows.Forms.TextBox();
+            this.subPanelLeft = new System.Windows.Forms.Panel();
+            this.centerPanel.SuspendLayout();
+            this.centerRightPanel.SuspendLayout();
+            this.subPanelWarning.SuspendLayout();
+            this.centerLeftPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.BackColor = System.Drawing.Color.Transparent;
+            this.leftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.leftPanel.Location = new System.Drawing.Point(0, 0);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Size = new System.Drawing.Size(200, 670);
+            this.leftPanel.TabIndex = 0;
+            // 
+            // rightPanel
+            // 
+            this.rightPanel.BackColor = System.Drawing.Color.Transparent;
+            this.rightPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.rightPanel.Location = new System.Drawing.Point(770, 0);
+            this.rightPanel.Name = "rightPanel";
+            this.rightPanel.Size = new System.Drawing.Size(200, 670);
+            this.rightPanel.TabIndex = 1;
+            // 
+            // topPanel
+            // 
+            this.topPanel.BackColor = System.Drawing.Color.Transparent;
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(200, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(570, 200);
+            this.topPanel.TabIndex = 2;
+            // 
+            // bottomPanel
+            // 
+            this.bottomPanel.BackColor = System.Drawing.Color.Transparent;
+            this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bottomPanel.Location = new System.Drawing.Point(200, 470);
+            this.bottomPanel.Name = "bottomPanel";
+            this.bottomPanel.Size = new System.Drawing.Size(570, 200);
+            this.bottomPanel.TabIndex = 3;
+            // 
+            // centerPanel
+            // 
+            this.centerPanel.BackColor = System.Drawing.Color.Transparent;
+            this.centerPanel.Controls.Add(this.btValideCategorie);
+            this.centerPanel.Controls.Add(this.centerRightPanel);
+            this.centerPanel.Controls.Add(this.centerLeftPanel);
+            this.centerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.centerPanel.Location = new System.Drawing.Point(200, 200);
+            this.centerPanel.Name = "centerPanel";
+            this.centerPanel.Size = new System.Drawing.Size(570, 270);
+            this.centerPanel.TabIndex = 4;
+            // 
+            // btValideCategorie
+            // 
+            this.btValideCategorie.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(19)))), ((int)(((byte)(43)))));
+            this.btValideCategorie.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btValideCategorie.FlatAppearance.BorderSize = 0;
+            this.btValideCategorie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btValideCategorie.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btValideCategorie.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(33)))), ((int)(((byte)(61)))));
+            this.btValideCategorie.Location = new System.Drawing.Point(200, 230);
+            this.btValideCategorie.Name = "btValideCategorie";
+            this.btValideCategorie.Size = new System.Drawing.Size(170, 40);
+            this.btValideCategorie.TabIndex = 2;
+            this.btValideCategorie.Text = "AJOUTER";
+            this.btValideCategorie.UseVisualStyleBackColor = false;
+            this.btValideCategorie.Click += new System.EventHandler(this.btValideCategorie_Click);
+            // 
+            // centerRightPanel
+            // 
+            this.centerRightPanel.BackColor = System.Drawing.Color.Transparent;
+            this.centerRightPanel.Controls.Add(this.metroLabel2);
+            this.centerRightPanel.Controls.Add(this.inputPassword);
+            this.centerRightPanel.Controls.Add(this.subPanelWarning);
+            this.centerRightPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.centerRightPanel.Location = new System.Drawing.Point(370, 0);
+            this.centerRightPanel.Name = "centerRightPanel";
+            this.centerRightPanel.Size = new System.Drawing.Size(200, 270);
+            this.centerRightPanel.TabIndex = 1;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.metroLabel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.metroLabel2.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.metroLabel2.Location = new System.Drawing.Point(0, 89);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(200, 59);
+            this.metroLabel2.TabIndex = 3;
+            this.metroLabel2.Text = "MOT DE PASSE";
+            this.metroLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroLabel2.UseCustomBackColor = true;
+            this.metroLabel2.UseCustomForeColor = true;
+            // 
+            // inputPassword
+            // 
+            this.inputPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(19)))), ((int)(((byte)(43)))));
+            this.inputPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputPassword.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.inputPassword.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.inputPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.inputPassword.Location = new System.Drawing.Point(0, 148);
+            this.inputPassword.Name = "inputPassword";
+            this.inputPassword.PasswordChar = '*';
+            this.inputPassword.Size = new System.Drawing.Size(200, 22);
+            this.inputPassword.TabIndex = 2;
+            this.inputPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.inputPassword.UseSystemPasswordChar = true;
+            // 
+            // subPanelWarning
+            // 
+            this.subPanelWarning.BackColor = System.Drawing.Color.Transparent;
+            this.subPanelWarning.Controls.Add(this.labelWarning);
+            this.subPanelWarning.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.subPanelWarning.Location = new System.Drawing.Point(0, 170);
+            this.subPanelWarning.Name = "subPanelWarning";
+            this.subPanelWarning.Size = new System.Drawing.Size(200, 100);
+            this.subPanelWarning.TabIndex = 0;
+            // 
+            // labelWarning
+            // 
+            this.labelWarning.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.labelWarning.Location = new System.Drawing.Point(0, 0);
+            this.labelWarning.Name = "labelWarning";
+            this.labelWarning.Size = new System.Drawing.Size(200, 100);
+            this.labelWarning.TabIndex = 0;
+            this.labelWarning.Text = "Pour plus de sécurité votre mot de passe doit contenir au minimum une majuscule d" +
+    "es chiffres et des caractères alpha numérique";
+            this.labelWarning.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelWarning.UseCustomBackColor = true;
+            this.labelWarning.UseCustomForeColor = true;
+            this.labelWarning.Visible = false;
+            this.labelWarning.WrapToLine = true;
+            // 
+            // centerLeftPanel
+            // 
+            this.centerLeftPanel.BackColor = System.Drawing.Color.Transparent;
+            this.centerLeftPanel.Controls.Add(this.metroLabel1);
+            this.centerLeftPanel.Controls.Add(this.inputLogin);
+            this.centerLeftPanel.Controls.Add(this.subPanelLeft);
+            this.centerLeftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.centerLeftPanel.Location = new System.Drawing.Point(0, 0);
+            this.centerLeftPanel.Name = "centerLeftPanel";
+            this.centerLeftPanel.Size = new System.Drawing.Size(200, 270);
+            this.centerLeftPanel.TabIndex = 0;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.metroLabel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.metroLabel1.Location = new System.Drawing.Point(0, 89);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(200, 59);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "IDENFIANT";
+            this.metroLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroLabel1.UseCustomBackColor = true;
+            this.metroLabel1.UseCustomForeColor = true;
+            // 
+            // inputLogin
+            // 
+            this.inputLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(19)))), ((int)(((byte)(43)))));
+            this.inputLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputLogin.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.inputLogin.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.inputLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.inputLogin.Location = new System.Drawing.Point(0, 148);
+            this.inputLogin.Name = "inputLogin";
+            this.inputLogin.Size = new System.Drawing.Size(200, 22);
+            this.inputLogin.TabIndex = 1;
+            this.inputLogin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // subPanelLeft
+            // 
+            this.subPanelLeft.BackColor = System.Drawing.Color.Transparent;
+            this.subPanelLeft.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.subPanelLeft.Location = new System.Drawing.Point(0, 170);
+            this.subPanelLeft.Name = "subPanelLeft";
+            this.subPanelLeft.Size = new System.Drawing.Size(200, 100);
+            this.subPanelLeft.TabIndex = 0;
+            // 
+            // CategorieForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 670);
+            this.Controls.Add(this.centerPanel);
+            this.Controls.Add(this.bottomPanel);
+            this.Controls.Add(this.topPanel);
+            this.Controls.Add(this.rightPanel);
+            this.Controls.Add(this.leftPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CategorieForm";
+            this.Text = "CategorieForm";
+            this.centerPanel.ResumeLayout(false);
+            this.centerRightPanel.ResumeLayout(false);
+            this.centerRightPanel.PerformLayout();
+            this.subPanelWarning.ResumeLayout(false);
+            this.centerLeftPanel.ResumeLayout(false);
+            this.centerLeftPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel leftPanel;
+        private System.Windows.Forms.Panel rightPanel;
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.Panel bottomPanel;
+        private System.Windows.Forms.Panel centerPanel;
+        private System.Windows.Forms.Panel centerRightPanel;
+        private System.Windows.Forms.Panel centerLeftPanel;
+        private System.Windows.Forms.Panel subPanelWarning;
+        private System.Windows.Forms.Panel subPanelLeft;
+        private System.Windows.Forms.TextBox inputPassword;
+        private System.Windows.Forms.TextBox inputLogin;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private System.Windows.Forms.Button btValideCategorie;
+        private MetroFramework.Controls.MetroLabel labelWarning;
+    }
+}
