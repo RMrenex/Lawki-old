78f608112ba851aaf9e662150887d1c9c2c26112
Erem <rmrenex@gmail.com>
2021-03-31 23:04:35 +0200

    Add static list to get categories

--- models/Categorie.cs
+++ models/Categorie.cs
@@ -8,6 +8,7 @@ namespace LowaPasswd.models
 {
     class Categorie
     {
+        private static List<Categorie> categories_ = new List<Categorie>();
         private string name_;
         private List<Credential> credentials_;
 
@@ -17,7 +18,9 @@ namespace LowaPasswd.models
             this.Credentials_ = credentials;
         }
 
+        internal static List<Categorie> Categories_ { get => categories_; }
         public string Name_ { get => name_; set => name_ = value; }
         internal List<Credential> Credentials_ { get => credentials_; set => credentials_ = value; }
+        
     }
 }
