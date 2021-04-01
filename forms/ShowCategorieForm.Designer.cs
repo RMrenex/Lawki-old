3c3f04566221382ee8c70354dbf47b5e5150c424
Erem <rmrenex@gmail.com>
2021-04-01 23:26:06 +0200

    Create new Form

---
+++ forms/ShowCategorieForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace LowaPasswd.forms
+{
+    partial class ShowCategorieForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.bottomPanel = new System.Windows.Forms.Panel();
+            this.leftPanel = new System.Windows.Forms.Panel();
+            this.rightPanel = new System.Windows.Forms.Panel();
+            this.flowLayoutContentPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // topPanel
+            // 
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(970, 80);
+            this.topPanel.TabIndex = 0;
+            // 
+            // bottomPanel
+            // 
+            this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bottomPanel.Location = new System.Drawing.Point(0, 590);
+            this.bottomPanel.Name = "bottomPanel";
+            this.bottomPanel.Size = new System.Drawing.Size(970, 80);
+            this.bottomPanel.TabIndex = 1;
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.leftPanel.Location = new System.Drawing.Point(0, 80);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Size = new System.Drawing.Size(80, 510);
+            this.leftPanel.TabIndex = 2;
+            // 
+            // rightPanel
+            // 
+            this.rightPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.rightPanel.Location = new System.Drawing.Point(890, 80);
+            this.rightPanel.Name = "rightPanel";
+            this.rightPanel.Size = new System.Drawing.Size(80, 510);
+            this.rightPanel.TabIndex = 3;
+            // 
+            // flowLayoutContentPanel
+            // 
+            this.flowLayoutContentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutContentPanel.Location = new System.Drawing.Point(80, 80);
+            this.flowLayoutContentPanel.Name = "flowLayoutContentPanel";
+            this.flowLayoutContentPanel.Size = new System.Drawing.Size(810, 510);
+            this.flowLayoutContentPanel.TabIndex = 4;
+            // 
+            // ShowCategorieForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 670);
+            this.Controls.Add(this.flowLayoutContentPanel);
+            this.Controls.Add(this.rightPanel);
+            this.Controls.Add(this.leftPanel);
+            this.Controls.Add(this.bottomPanel);
+            this.Controls.Add(this.topPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ShowCategorieForm";
+            this.Text = "ShowCategorieForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.Panel bottomPanel;
+        private System.Windows.Forms.Panel leftPanel;
+        private System.Windows.Forms.Panel rightPanel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutContentPanel;
+    }
+}
