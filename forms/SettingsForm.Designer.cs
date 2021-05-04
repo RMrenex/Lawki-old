1e6b8ea1ff52d7dca8d1abfbd31388177ef37488
Erem <rmrenex@gmail.com>
2021-05-04 23:08:19 +0200

    Add open file dialog and design

--- forms/SettingsForm.Designer.cs
+++ forms/SettingsForm.Designer.cs
@@ -29,21 +29,163 @@ namespace LowaPass.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelSetupFile = new System.Windows.Forms.Label();
+            this.labelLang = new System.Windows.Forms.Label();
+            this.labelTheme = new System.Windows.Forms.Label();
+            this.comboBoxLang = new MetroFramework.Controls.MetroComboBox();
+            this.inputFileSetup = new System.Windows.Forms.TextBox();
+            this.metroComboBoxTheme = new MetroFramework.Controls.MetroComboBox();
+            this.buttonSearchFile = new System.Windows.Forms.Button();
+            this.buttonSaveChanged = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // labelSetupFile
+            // 
+            this.labelSetupFile.BackColor = System.Drawing.Color.Transparent;
+            this.labelSetupFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSetupFile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.labelSetupFile.Location = new System.Drawing.Point(238, 255);
+            this.labelSetupFile.Name = "labelSetupFile";
+            this.labelSetupFile.Size = new System.Drawing.Size(264, 29);
+            this.labelSetupFile.TabIndex = 0;
+            this.labelSetupFile.Text = "Changer le répertoire d\'installation :";
+            this.labelSetupFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelLang
+            // 
+            this.labelLang.BackColor = System.Drawing.Color.Transparent;
+            this.labelLang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLang.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.labelLang.Location = new System.Drawing.Point(238, 290);
+            this.labelLang.Name = "labelLang";
+            this.labelLang.Size = new System.Drawing.Size(264, 29);
+            this.labelLang.TabIndex = 0;
+            this.labelLang.Text = "Choisir la langue :";
+            this.labelLang.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelTheme
+            // 
+            this.labelTheme.BackColor = System.Drawing.Color.Transparent;
+            this.labelTheme.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTheme.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.labelTheme.Location = new System.Drawing.Point(238, 325);
+            this.labelTheme.Name = "labelTheme";
+            this.labelTheme.Size = new System.Drawing.Size(264, 29);
+            this.labelTheme.TabIndex = 0;
+            this.labelTheme.Text = "Changer le thème :";
+            this.labelTheme.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // comboBoxLang
+            // 
+            this.comboBoxLang.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(19)))), ((int)(((byte)(43)))));
+            this.comboBoxLang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBoxLang.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.comboBoxLang.FormattingEnabled = true;
+            this.comboBoxLang.ItemHeight = 23;
+            this.comboBoxLang.Items.AddRange(new object[] {
+            "Francais",
+            "Anglais"});
+            this.comboBoxLang.Location = new System.Drawing.Point(524, 290);
+            this.comboBoxLang.Name = "comboBoxLang";
+            this.comboBoxLang.PromptText = "Sélection langue";
+            this.comboBoxLang.Size = new System.Drawing.Size(140, 29);
+            this.comboBoxLang.TabIndex = 1;
+            this.comboBoxLang.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.comboBoxLang.UseCustomBackColor = true;
+            this.comboBoxLang.UseCustomForeColor = true;
+            this.comboBoxLang.UseSelectable = true;
+            // 
+            // inputFileSetup
+            // 
+            this.inputFileSetup.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(19)))), ((int)(((byte)(43)))));
+            this.inputFileSetup.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputFileSetup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputFileSetup.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.inputFileSetup.Location = new System.Drawing.Point(524, 255);
+            this.inputFileSetup.Multiline = true;
+            this.inputFileSetup.Name = "inputFileSetup";
+            this.inputFileSetup.ReadOnly = true;
+            this.inputFileSetup.Size = new System.Drawing.Size(140, 29);
+            this.inputFileSetup.TabIndex = 2;
+            this.inputFileSetup.WordWrap = false;
+            // 
+            // metroComboBoxTheme
+            // 
+            this.metroComboBoxTheme.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(19)))), ((int)(((byte)(43)))));
+            this.metroComboBoxTheme.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.metroComboBoxTheme.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.metroComboBoxTheme.FormattingEnabled = true;
+            this.metroComboBoxTheme.ItemHeight = 23;
+            this.metroComboBoxTheme.Items.AddRange(new object[] {
+            "Défaut",
+            "Blanc",
+            "Dark"});
+            this.metroComboBoxTheme.Location = new System.Drawing.Point(524, 325);
+            this.metroComboBoxTheme.Name = "metroComboBoxTheme";
+            this.metroComboBoxTheme.PromptText = "Sélection thème";
+            this.metroComboBoxTheme.Size = new System.Drawing.Size(140, 29);
+            this.metroComboBoxTheme.TabIndex = 1;
+            this.metroComboBoxTheme.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroComboBoxTheme.UseCustomBackColor = true;
+            this.metroComboBoxTheme.UseCustomForeColor = true;
+            this.metroComboBoxTheme.UseSelectable = true;
+            // 
+            // buttonSearchFile
+            // 
+            this.buttonSearchFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearchFile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.buttonSearchFile.Location = new System.Drawing.Point(670, 255);
+            this.buttonSearchFile.Name = "buttonSearchFile";
+            this.buttonSearchFile.Size = new System.Drawing.Size(29, 29);
+            this.buttonSearchFile.TabIndex = 3;
+            this.buttonSearchFile.Text = "...";
+            this.buttonSearchFile.UseVisualStyleBackColor = true;
+            this.buttonSearchFile.Click += new System.EventHandler(this.buttonSearchFile_Click);
+            // 
+            // buttonSaveChanged
+            // 
+            this.buttonSaveChanged.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSaveChanged.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSaveChanged.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(163)))), ((int)(((byte)(17)))));
+            this.buttonSaveChanged.Location = new System.Drawing.Point(362, 411);
+            this.buttonSaveChanged.Name = "buttonSaveChanged";
+            this.buttonSaveChanged.Size = new System.Drawing.Size(140, 29);
+            this.buttonSaveChanged.TabIndex = 4;
+            this.buttonSaveChanged.Text = "Sauvergarder";
+            this.buttonSaveChanged.UseVisualStyleBackColor = true;
+            this.buttonSaveChanged.Click += new System.EventHandler(this.buttonSaveChanged_Click);
             // 
             // SettingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(33)))), ((int)(((byte)(61)))));
             this.ClientSize = new System.Drawing.Size(970, 670);
+            this.Controls.Add(this.buttonSaveChanged);
+            this.Controls.Add(this.buttonSearchFile);
+            this.Controls.Add(this.inputFileSetup);
+            this.Controls.Add(this.metroComboBoxTheme);
+            this.Controls.Add(this.comboBoxLang);
+            this.Controls.Add(this.labelTheme);
+            this.Controls.Add(this.labelLang);
+            this.Controls.Add(this.labelSetupFile);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "SettingsForm";
             this.Text = "SettingsForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelSetupFile;
+        private System.Windows.Forms.Label labelLang;
+        private System.Windows.Forms.Label labelTheme;
+        private MetroFramework.Controls.MetroComboBox comboBoxLang;
+        private System.Windows.Forms.TextBox inputFileSetup;
+        private MetroFramework.Controls.MetroComboBox metroComboBoxTheme;
+        private System.Windows.Forms.Button buttonSearchFile;
+        private System.Windows.Forms.Button buttonSaveChanged;
     }
 }
--- forms/SettingsForm.cs
+++ forms/SettingsForm.cs
@@ -1,20 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace LowaPass.Forms
-{
+namespace LowaPass.Forms {
     public partial class SettingsForm : Form
     {
         public SettingsForm()
         {
             InitializeComponent();
+        }
+
+        private void buttonSearchFile_Click(object sender, EventArgs e) {
+
+            using (var fbd = new FolderBrowserDialog()) {
+
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath)) {
+                    inputFileSetup.Text = fbd.SelectedPath;
+                }
+            }
+
+        }
+
+        private void buttonSaveChanged_Click(object sender, EventArgs e) {
+
         }
     }
 }
