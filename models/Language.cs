76a048f5490f3a7eaa18ad8c449cd953ef9e0a9c
Erem <rmrenex@gmail.com>
2021-05-08 00:08:09 +0200

    Add new class

---
+++ models/Language.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowaPasswd.models {
+    class Language {
+
+        public static string EMPTY_FIELD_FR = "Erreur : Un ou plusieurs champs sont vides";
+        public static string MULTI_LINES_FR = "Erreur : vous ne pouvez pas écrire sur plusieurs lignes";
+        public static string NAME_ALREADY_USE_FR = "Erreur : Ce nom est déjà utilisé";
+        public static string EMPTY_FIELD_EN = "Error : One or more fields are empty";
+        public static string MULTI_LINES_EN = "Error: you cannot write on several lines";
+        public static string NAME_ALREADY_USE_EN = "Error: This name is already in use";
+
+    }
+}
