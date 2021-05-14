ef02543f8f68f3744022ab3bde8b98e032e432f4
Erem <rmrenex@gmail.com>
2021-05-15 01:49:10 +0200

    Initialize loading configuration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿using LowaPass;
+using LowaPasswd.models;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace LowaPasswd {
     static class Program
@@ -9,17 +13,43 @@ namespace LowaPasswd {
         /// <summary>
         /// Point d'entrée principal de l'application.
         /// </summary>
+        /// 
+        private static string directory;
+        private static string lang;
+        private static string theme;
+        private static XDocument lang_file;
+        private static Dictionary<string, string> xmlFields = new Dictionary<string, string>();
+
         [STAThread]
         static void Main()
         {
-            string firstStart = ConfigurationManager.AppSettings.Get("first");
-            string directory = ConfigurationManager.AppSettings.Get("directory");
-            string lang = ConfigurationManager.AppSettings.Get("lang");
-            string theme = ConfigurationManager.AppSettings.Get("theme");
+            FileManager.setupApplicationFiles();
+
+            if (ConfigurationManager.AppSettings.Get("first").Equals("true")) {
+
+                directory = ConfigurationManager.AppSettings.Get("directory");
+                lang = ConfigurationManager.AppSettings.Get("lang");
+
+                if (lang.Equals("fr")) {
+                    lang_file = XDocument.Load(FileManager.applicationDirectoryPath + "/lang/Fr.xml");
+                }
+                else {
+                    lang_file = XDocument.Load(FileManager.applicationDirectoryPath + "/lang/En.xml");
+                }
+
+                xmlFields = lang_file.Descendants("add").ToDictionary(key => key.Attribute("key").Value, value => value.Attribute("value").Value);
+                Theme = ConfigurationManager.AppSettings.Get("theme");
+            }
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
+
+        public static string Directory { get => directory; set => directory = value; }
+        public static string Lang { get => lang; set => lang = value; }
+        public static string Theme { get => theme; set => theme = value; }
+        public static Dictionary<string, string> Fields { get => xmlFields; set => xmlFields = value; }
+        public static XDocument Lang_file { get => lang_file; set => lang_file = value; }
     }
 }
